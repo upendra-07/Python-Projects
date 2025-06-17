380c916139805a22ca2021b415d2166c8d117275
Upendra <upendrag2815@gmail.com>
2025-06-17 08:18:47 +0530

    Urls and views

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,6 +2,6 @@ from django.urls import path;
 from . import views
 
 urlpatterns = [
-    path('january', views.index),
-    path('february', views.february)
+    path('<int:month>', views.monthly_challenges_by_number),
+    path('<str:month>', views.monthly_challenges)
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
-def index(request):
-    return HttpResponse('This works')
+def monthly_challenges_by_number(request, month):
+    return HttpResponse(month)
 
-def february(request):
-    return HttpResponse("Welcome to Feb!!")
+def monthly_challenges(request, month):
+    challenges_text = None
+    if (month == 'january'):
+        challenges_text = 'Welcome to January!!'
+    elif (month == 'february'):
+        challenges_text = 'Walk for atleast 20min every day'
+    elif (month == 'march'):
+        challenges_text = 'Learn Django for atleast 20 minutes every day!!'
+    else:
+        return HttpResponseNotFound('This month is not supported')
+    return HttpResponse(challenges_text)
